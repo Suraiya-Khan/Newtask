cbb904c22c38d22a06fe7610e0cc7c0dcae45cfb
Suraiya Khan <suraiya.khan130@gmail.com>
2023-10-15 18:27:24 +0530

    edited

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let calculator_input_Number_V = document.getElementById('calculator_input_Number');
 let calculator_input_Number_Previews_V = document.getElementById('calculator_input_Number_Previews');
 
@@ -26,37 +26,4 @@ function displaySolve(){
     let y= eval(eq);
 
     calculator_input_Number_V.value=y;
-
-
-=======
-let calculator_input_Number_V = document.getElementById('calculator_input_Number');
-let calculator_input_Number_Previews_V = document.getElementById('calculator_input_Number_Previews');
-
-let Calculator_input_AC_V = document.getElementById('Calculator_input_AC');
-
-let DisplayValue;
-
-function display(DisplayValue){
-    calculator_input_Number_V.value+=DisplayValue;
-
-}
-
-Calculator_input_AC_V.addEventListener('click',()=>{
-    calculator_input_Number_V.value="";
-    calculator_input_Number_Previews_V.value="";
-
-})
-
-function displaySolve(){
-
-    let eq = calculator_input_Number_V.value;
-
-    calculator_input_Number_Previews_V.value=eq;
-
-    let y= eval(eq);
-
-    calculator_input_Number_V.value=y;
-
-
->>>>>>> af433d9f59bc5d95a73b06d256cf50f7800ac737
 }
